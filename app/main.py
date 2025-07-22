3d479b228bdc70d0e37e347261256f914be65596
kranthimj23 <kranthimj23@gmail.com>
2025-07-22 15:33:23 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ def write():
     return jsonify({"msg": "Data written to Aerospike and PostgreSQL"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8080)
